ea45f87a41b8996ce0d13b167d4e254dff95cf0e
Priyank Thakkar <priyankthakkar@gmail.com>
2017-09-06 22:49:29 +1000

    Change Log: 1. Added a class with main method which is the entry point for SpringBoot application. 2. Modified .gitignore file for ignoring target folder Changes By: Priyank Thakkar

---
+++ src/main/java/io/java/springbootquickstart/CourseAPIApp.java
@@ -0,0 +1,15 @@
+package io.java.springbootquickstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by priyankthakkar on 6/9/17.
+ */
+@SpringBootApplication
+public class CourseAPIApp {
+
+    public static void main(String[] args){
+        SpringApplication.run(CourseAPIApp.class, args);
+    }
+}
